b2212549148fff9495ac1ad0fbb54d71c8442b7c
Josh Egan <josh.egan145@gmail.com>
2015-05-21 20:27:48 -0600

    Refactor and slight style changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,17 @@
-(function(){
+(function () {
   "use strict";
 
   var qs = document.querySelector.bind(document);
-  
-  qs('.start-pomodoro').addEventListener('click', function(){
+
+  qs('.start-pomodoro').addEventListener('click', function () {
     alert("pomodoro started!");
   });
 
-  qs('.start-short-break').addEventListener('click', function(){
+  qs('.start-short-break').addEventListener('click', function () {
     alert("short break started!");
   });
 
-  qs('.start-long-break').addEventListener('click', function(){
+  qs('.start-long-break').addEventListener('click', function () {
     alert("long break started!");
   });
 
